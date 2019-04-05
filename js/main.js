ce8d9afe18eae668f2ae0fe57f3b959490629e2b
mpetty <michael.petty@gmail.com>
2019-04-05 15:46:08 -0700

    start carousel

--- js/main.js
+++ js/main.js
@@ -8,3 +8,13 @@ let wordFadeToggle = (elem) => {
 setInterval(wordFadeToggle, 3000, document.getElementById('splash1'));
 setTimeout(setInterval, 3000, wordFadeToggle, 3000, document.getElementById('splash2'));
 setTimeout(setInterval, 5000, wordFadeToggle, 3000, document.getElementById('splash3'));
+
+
+// carousel
+const slides = [1,2,3,4,5,6];
+
+let slideCount = slides.length;
+for (i = 0; i < slideCount; i++) {
+    setTimeout( () => {(console.log(`slide: ${slides[i]}`))}, 2000);
+
+}
