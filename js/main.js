e0e8882044df3fe44aa271b8f7c1997ce56ad494
mpetty <michael.petty@gmail.com>
2019-04-04 22:53:57 -0700

    add interval for splash; add google fonts

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-document.querySelector('body').addEventListener('click', () => {
-  console.log('clicked body');
-})
+// document.querySelector('body').addEventListener('click', () => {
+//   document.getElementById('splash1').classList.toggle("fadeIn");
+// })
+
+let wordFadeToggle = (elem) => {
+  elem.classList.toggle("fadeIn");
+}
+setInterval(wordFadeToggle, 3000, document.getElementById('splash1'));
+setTimeout(setInterval, 3000, wordFadeToggle, 3000, document.getElementById('splash2'));
+setTimeout(setInterval, 5000, wordFadeToggle, 3000, document.getElementById('splash3'));
