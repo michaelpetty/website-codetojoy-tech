449353d580a8cfc3cc03bb861408ff3a5b323090
mpetty <michael.petty@gmail.com>
2019-04-08 00:51:09 -0700

    dots change slide view

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@ let elemFadeToggle = (elem, setHidden) => {
 let navLinks = document.querySelectorAll(".linkBox a");
 
 window.addEventListener("scroll", event => {
-  let fromTop = window.scrollY;
+  let fromTop = window.scrollY + 100; //offset navbar
 
   navLinks.forEach(link => {
     let section = document.querySelector(link.hash);
@@ -36,14 +36,18 @@ setTimeout(setInterval, 5000, elemFadeToggle, 3000, document.getElementById('spl
 let slideGroup = document.querySelector('.slides');
 let slides = slideGroup.querySelectorAll('img');
 const slidesNum = slides.length;
+let dots = document.querySelectorAll('.dot');
 
 //show last slide under previous arrow on first slide
 slideGroup.prepend(slides[slidesNum-1]);
+let dotPos = 0;
 
 document.querySelector('.arrowNext').addEventListener('click', () => {
-  let rootCSS = window.getComputedStyle(document.documentElement);
-  let moveBy = rootCSS.getPropertyValue('--slideMove') * 1;
-  let slidePosition = rootCSS.getPropertyValue('--slideOffset') * 1;
+  //move dots
+  dots[dotPos].classList.remove('active');
+  dotPos++;
+  dotPos = (dotPos === slidesNum)? 0: dotPos;
+  dots[dotPos].classList.add('active');
 
   slideGroup = document.querySelector('.slides');
   slides = slideGroup.querySelectorAll('img');
@@ -51,13 +55,15 @@ document.querySelector('.arrowNext').addEventListener('click', () => {
   setTimeout(() => {
     slideGroup.append(slides[0]);
     slideGroup.classList.remove('nextSlide');
-  }, 2000);
+  }, 1000);
 })
 
 document.querySelector('.arrowPrev').addEventListener('click', () => {
-  let rootCSS = window.getComputedStyle(document.documentElement);
-  let moveBy = rootCSS.getPropertyValue('--slideMove') * 1;
-  let slidePosition = rootCSS.getPropertyValue('--slideOffset') * 1;
+  //move dots
+  dots[dotPos].classList.remove('active');
+  dotPos--;
+  dotPos = (dotPos < 0)? slidesNum-1: dotPos;
+  dots[dotPos].classList.add('active');
 
   slideGroup = document.querySelector('.slides');
   slides = slideGroup.querySelectorAll('img');
@@ -65,11 +71,34 @@ document.querySelector('.arrowPrev').addEventListener('click', () => {
   slideGroup.prepend(slides[slidesNum-1]);
   setTimeout(() => {
     slideGroup.classList.remove('prevSlide');
-  }, 2000);
+  }, 1000);
+})
+
+document.querySelector('.dots').addEventListener('click', (e) => {
+  if (e.target && e.target.matches("span.dot")) {
+    let reqSlide = e.target.dataset.slideId;
+    let slideHops = dotPos + 1 - reqSlide;
+    if (slideHops > 0) {
+      for (i=0; i < slideHops; i++) {
+        slideGroup = document.querySelector('.slides');
+        slides = slideGroup.querySelectorAll('img');
+        slideGroup.prepend(slides[slidesNum-1]);
+      }
+    } else if (slideHops < 0) {
+      for (i=0; i > slideHops; i--) {
+        slideGroup = document.querySelector('.slides');
+        slides = slideGroup.querySelectorAll('img');
+        slideGroup.append(slides[0]);
+      }
+    }
+    dots[dotPos].classList.remove('active');
+    dotPos = reqSlide - 1;
+    dots[dotPos].classList.add('active');
+  }
 })
 
 // about change header
-document.getElementById('about').addEventListener('mouseenter', () => {
+document.querySelector('.bigHead').addEventListener('mouseenter', () => {
   let aboutHead = document.querySelector('.bigHead');
   let aboutHTML = aboutHead.innerHTML;
   let tempHTML = document.querySelector('.resume-Am-I').innerHTML;
